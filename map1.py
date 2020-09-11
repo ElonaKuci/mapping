3a6ac8e5661f4c537f46620a525ebeaadf52bdb0
Elona Kuqi <elonakuqi@MacBook-Pro.local>
2020-09-11 18:50:18 +0200

    commit map1.py

--- map1.py
+++ map1.py
@@ -1,4 +1,33 @@
 
 import folium
-map= folium.Map(location=[42,20], tiles = "Stamen Terrain")
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
+name = list(data["NAME"])
+
+def color_producer(elevation):
+    if elevation<1000:
+        return "green"
+    elif 1000<=elevation<3000:
+        return "orange"
+    else:
+        return "red"
+
+html = """
+Volcano name:<br>
+<a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a><br>
+Height: %s m
+"""
+
+map = folium.Map(location=[38.58, -99.09], zoom_start=5, tiles="Mapbox Bright")
+fg = folium.FeatureGroup(name = "My Map")
+
+for lt, ln, el, name in zip(lat, lon, elev, name):
+    iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
+    fg.add_child(folium.CircleMarker(location=[lt, ln], popup=folium.Popup(iframe),radius=6,fill_color==color_producer(el),color="grey",fill_opacity=0.7))
+
+map.add_child(fg)
 map.save("map1.html")
